e6f879a1d40801aae46e4bc438741bb5fea61013
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2023-06-05 23:25:00 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <TheHeader v-if="showHeader" />
+
+  <div v-show="showName">
+    Nome: {{ firstName }}
+    <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'">Admin</div>
+  <div v-else-if="acessLevel === 'mkt'">Mkt</div>
+  <div v-else>User</div>
+
+  <div>
+    <div v-for="obj in todos" :key="obj.id" class="todos-item">
+      {{ obj.title }}
+    </div>
+  </div>
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    HelloWorld
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: "Leandro",
+      lastName: "Ferraz",
+      showName: true,
+      acessLevel: '',
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: "Automação"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "Programação"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "Qualidade"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ 
+.todos-item {
+  background-color: aqua;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+        Header 
+    </header>
+</template>
+  
+<script>
+export default {
+
+}
+</script>
+  
+ 
+<style scoped>
+    .header {
+        background-color: black;
+        color: beige;
+    }
+</style>
+  
